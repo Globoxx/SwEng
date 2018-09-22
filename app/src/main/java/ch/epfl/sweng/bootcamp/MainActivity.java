47c96fc20584becf0468fb9d5f88e811744a45bb
Vincent Gurtler <vincent.guertler@gmail.com>
2018-09-22 18:45:26 +0200

    Add the greetingActivity

--- app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
@@ -1,14 +1,21 @@
 package ch.epfl.sweng.bootcamp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String BUNDLE_EXTRA_NAME = "BUNDLE_EXTRA_NAME";
+
     private Button button;
-    private EditText text;
+
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.mainGoButton);
-        text = findViewById(R.id.mainName);
+        EditText nameEdit = findViewById(R.id.mainName);
+
+        button.setEnabled(false);
+
+        nameEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                name = s.toString();
+                button.setEnabled(count > 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent greetingActivity = new Intent(MainActivity.this, GreetingActivity.class);
+                greetingActivity.putExtra(BUNDLE_EXTRA_NAME, name);
+                startActivity(greetingActivity);
+            }
+        });
     }
 }
