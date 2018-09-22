efc69805fdcd08bfa39ad9d63a27de267d4b3051
Vincent Gurtler <vincent.guertler@gmail.com>
2018-09-22 16:08:29 +0200

    Added the travis yml file

--- app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
@@ -3,12 +3,12 @@ package ch.epfl.sweng.bootcamp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-    private TextView text;
+    private EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
